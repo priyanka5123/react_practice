1aed27b6183495442df92c1b6e0a2785c8449f20
priyanka5123 <priyanka.k8100@gmail.com>
2025-09-23 19:06:35 -0700

    First Commit

---
+++ react-todo-app/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>My First React App 🎉</h1>
+        <p>React is ready, let’s go 🚀</p>
+      </div>
+    </>
+  )
+}
+
+export default App
